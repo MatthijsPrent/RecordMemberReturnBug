fd40289054ef01e1ceee11d8d2ef6f9a9efbe1fa
Matthijs Prent <matthijs.prent@Applicos.com>
2022-04-11 16:34:52 +0200

    Add project files.

---
+++ Library.fs
@@ -0,0 +1,9 @@
+﻿namespace RecordMemberReturnBug
+open System
+type MyType () =
+    member this.NotWorking(value: obj) : obj =
+        sprintf "test: %.2f" 1.1
+
+    member this.Working(value: obj) : obj =
+        let a = sprintf "test: %.2f" 1.1
+        a
